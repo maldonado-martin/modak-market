02361fbac960a628bbb4d050835eaa0bba26a0c8
Martin Maldonado <maldonado.mg95@gmail.com>
2025-04-10 20:27:47 -0300

    add import/export sorter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   extends: ["expo", "prettier"],
-  plugins: ["prettier"],
+  plugins: ["prettier", "simple-import-sort"],
   rules: {
     "prettier/prettier": "error",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
   },
 };
